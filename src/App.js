ec6af6c71bf77856f99ecaf2f4420f97b45a79dc
bgunturk <bulent.gunturk@gmail.com>
2021-04-15 19:51:37 +0300

    initialcommit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './index.css';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="menu">25</div>
+      <div className="main">75</div>
+      <i className="far fa-heart"></i>
+    </div>
+  );
+};
+export default Header;
